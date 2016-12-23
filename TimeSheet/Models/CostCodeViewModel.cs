72053937a5a2f44a35566fcc8a4fa364469c0906
kjaek <kjaek@outlook.com>
2016-12-24 01:02:05 +1300

    moved cost code from admin to cost code controller (removed admin controller as its not used). added cost code view model + edit view

---
+++ TimeSheet/Models/CostCodeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace TimeSheet.Models
+{
+    public class CostCodeViewModel
+    {
+        public CostCodeViewModel()
+        {
+            Amount = 0;
+        }
+        [Required]
+        [DisplayName("Cost Code:")]
+        public string CostCode { get; set; }
+        [DisplayName("Comments:")]
+        public string Comments { get; set; }
+        [Required]
+        [DisplayName("Amount:")]
+        [DataType(DataType.Currency)]
+        public float? Amount { get; set; }
+        // TODO: drop down list of status
+        public string Status { get; set; }
+    }
+}
