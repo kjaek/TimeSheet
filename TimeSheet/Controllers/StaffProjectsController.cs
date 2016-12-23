21f8efbf2a7e663743f6fe78168c519bd79c3867
kjaek <kjaek@outlook.com>
2016-12-24 00:03:38 +1300

    return blank model for staff project to return default charge and cost rate in controller

--- TimeSheet/Controllers/StaffProjectsController.cs
+++ TimeSheet/Controllers/StaffProjectsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TimeSheet.Models;
 
 namespace TimeSheet.Controllers
 {
@@ -16,7 +17,7 @@ namespace TimeSheet.Controllers
 
         public ActionResult Edit()
         {
-            return View();
+            return View(new StaffProjectViewModel());
         }
     }
 }
