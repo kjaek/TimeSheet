4aaf9d9f20ba071c2d3f9e04f1546b80367ef01c
kjaek <kjaek@outlook.com>
2016-12-24 21:51:24 +1300

    Added service layer for project and project tasks

---
+++ TimeSheet.Service/ProjectService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeSheet.Data.Infrastructure;
+using TimeSheet.Model;
+using TimeSheet.Data.Repositories;
+
+namespace TimeSheet.Service
+{
+    public interface IProjectService
+    {
+        Project GetProject(int id);
+        IEnumerable<Project> GetProjects();
+        void CreateProject(Project project);
+        void SaveProject();
+    }
+
+    public class ProjectService : IProjectService
+    {
+        private readonly IProjectRepository projectRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public ProjectService(IProjectRepository projectRepository, IUnitOfWork unitOfWork)
+        {
+            this.projectRepository = projectRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        #region Implementation
+        public IEnumerable<Project> GetProjects()
+        {
+            var projects = projectRepository.GetAll();
+            return projects;
+        }
+
+        public Project GetProject(int id)
+        {
+            var project = projectRepository.GetById(id);
+            return project;
+        }
+
+        public void CreateProject(Project project)
+        {
+            projectRepository.Add(project);
+        }
+
+        public void SaveProject()
+        {
+            unitOfWork.Commit();
+        }
+        #endregion  
+    }
+}
---
+++ TimeSheet.Service/ProjectTaskService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeSheet.Data.Infrastructure;
+using TimeSheet.Data.Repositories;
+using TimeSheet.Model;
+
+namespace TimeSheet.Service
+{
+    public interface IProjectTaskService
+    {
+        IEnumerable<ProjectTask> GetProjectTasks();
+        ProjectTask GetProjectTask(int id);
+        void CreateProjectTask(ProjectTask projectTask);
+        void SaveProjectTask();
+    }
+
+    public class ProjectTaskService : IProjectTaskService
+    {
+        private readonly IProjectTaskRepository projectTaskRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public ProjectTaskService(IProjectTaskRepository projectTaskRepository, IUnitOfWork unitOfWork)
+        {
+            this.projectTaskRepository = projectTaskRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        #region Implementation
+        public IEnumerable<ProjectTask> GetProjectTasks()
+        {
+            var projectTasks = projectTaskRepository.GetAll();
+            return projectTasks;
+        }
+
+        public ProjectTask GetProjectTask(int id)
+        {
+            var projectTask = projectTaskRepository.GetById(id);
+            return projectTask;
+        }
+
+        public void CreateProjectTask(ProjectTask projectTask)
+        {
+            projectTaskRepository.Add(projectTask);
+        }
+
+        public void SaveProjectTask()
+        {
+            unitOfWork.Commit();
+        }
+        #endregion
+    }
+}
